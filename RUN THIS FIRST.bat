1b2833a7c02180bc1c162a04e92727dfda415b49
reiusx1 <103373997+andreiteurpe@users.noreply.github.com>
2022-04-24 16:50:09 +0200

    Add files via upload

---
+++ RUN THIS FIRST.bat
@@ -0,0 +1,9 @@
+@echo off
+echo [40;32mInstalling Requirements For You....[40;37m
+echo.
+pip install --upgrade -r requirements.txt
+echo.
+echo [40;32mLaunching Increase bot..[40;37m
+timeout 2 >nul
+start increasebot.exe
+start tiktoksockets.exe
